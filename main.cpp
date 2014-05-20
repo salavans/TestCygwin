fbdf332482c5eed0ea9eca650f42313ec00473af
salavans <salavan30@gmail.com>
2014-05-20 06:54:13 +0530

    Client asked me to change a comment Exaxct change paragraph *client asked for the change
    Closes-Bug: #12

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: salavans
+ *
+ * Created on May 16, 2014, 11:42 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+ //This is a Sample One
+int main(int argc, char** argv) {
+   cout << "Hello World" << endl;
+    return 0;
+}
+
